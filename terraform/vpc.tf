21c3e46aa9a2779cbac7cfb4231c70b033a10960
pranob0x02 <pranobkantinath@gmail.com>
2025-06-18 00:51:36 +0300

    githubActions with terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,11 +4,11 @@ module "vpc" {
 
   name = "vprofile-eks"
 
-  cidr = "172.20.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  cidr = var.cidr
+  azs  = var.azs
 
-  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
-  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
   enable_nat_gateway   = true
   single_nat_gateway   = true
